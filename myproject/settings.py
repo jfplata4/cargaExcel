932d779109437c2beb60b76b8405939d616f637d
Julian <jfplata48@ucatolica.edu.co>
2024-11-05 17:26:03 -0500

    third

--- myproject/settings.py
+++ myproject/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/home/jfplata4/cargaExcel/static'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/jfplata/mySiteTest/static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/jfplata/mySiteTest/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
