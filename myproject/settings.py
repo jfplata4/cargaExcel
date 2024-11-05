a3f79a7aa91256ee8651ebdafdee91f67b15ce37
Julian <jfplata48@ucatolica.edu.co>
2024-11-05 16:32:29 -0500

    second

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fc9d)%c@l(e#4&f3tbr-aqr-#7&y8&yg%^)-cz_4vac05j%l8f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jfplata.pythonanywhere.com']
 
 
 # Application definition
